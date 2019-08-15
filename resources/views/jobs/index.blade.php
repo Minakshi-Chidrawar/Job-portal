7718fd006c589448cae3736b9f6508a151e01105
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-08-15 21:00:05 +0100

    View to list the jobs

---
+++ resources/views/jobs/index.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <h1>Recent Jobs</h1>
+        <table class="table">
+            <thead>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+            </thead>
+            <tbody>
+                @foreach($jobs as $job)
+                <tr>
+                    <td><img src="{{ asset('avatar/rose.jpg')}}" alt=""></td>
+                    <td>Position:{{ $job->position }}
+                        <br>
+                        <i class="fa fa-clock" aria-hidden="true"></i>&nbsp;&nbsp;fulltime
+                    </td>
+                    <td><i class="fa fa-map-marker" aria-hidden="true"></i>Address:{{ $job->address }}</td>
+                    <td><i class="fa fa-globe" aria-hidden="true"></i>Date:{{ $job->created_at }}</td>
+                    <td><button class="btn btn-success btn-sm">Apply</button></td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+    </div>
+</div>
+@endsection
