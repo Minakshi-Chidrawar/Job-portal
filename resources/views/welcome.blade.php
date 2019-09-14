ff3baf72a1b26bdfca46e716826566ef28e86be0
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-09-14 22:30:18 +0100

    Added font classes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,12 +13,15 @@
                 <th></th>
             </thead>
             <tbody>
-                @for($i=0; $i<10; $i++)
+                @for($i=0; $i<=10; $i++)
                 <tr>
                     <td><img src="{{ asset('avatar/rose.jpg')}}" alt=""></td>
-                    <td>Position: Web developer</td>
+                    <td>Position: Web developer
+                        <br>
+                        <i class="fa fa-clock" aria-hidden="true"></i>&nbsp;&nbsp;fulltime
+                    </td>
                     <td><i class="fa fa-map-marker" aria-hidden="true"></i>Address: Melbourne</td>
-                    <td>Date: 2019-08-14</td>
+                    <td><i class="fa fa-globe" aria-hidden="true"></i>Date: 2019-08-14</td>
                     <td><button class="btn btn-success btn-sm">Apply</button></td>
                 </tr>
                 @endfor
