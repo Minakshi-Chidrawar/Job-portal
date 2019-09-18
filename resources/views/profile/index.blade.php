6b7b5ec5c04db28daf7cc7baea0d5cfce1413e40
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-09-18 20:43:30 +0100

    Amended to add the action to update user profile

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -9,28 +9,34 @@
         <div class="col-md-6">
             <div class="card">
                 <div class="card-header">Update Your Profile</div>
+                <form action="{{ route('profile.create') }}" method="POST">
+                    @csrf
+                    <div class="card-body">
+                        <div class="form-group">
+                            <label for="">Address</label>
+                            <input type="text" class="form-control" name="address">
+                        </div>
 
-                <div class="card-body">
-                    <div class="form-group">
-                        <label for="">Address</label>
-                        <input type="text" class="form-control" name="address">
+                        <div class="form-group">
+                            <label for="">Experience</label>
+                            <textarea name="experience" class="form-control" id="" cols="30" rows="10"></textarea>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="">Bio</label>
+                            <textarea name="bio" class="form-control" id="" cols="30" rows="10"></textarea>
+                        </div>
+
+                        <div class="form-group">
+                            <button class="btn btn-success" type="submit">Update</button>
+                        </div>                
                     </div>
-
-                    <div class="form-group">
-                        <label for="">Experience</label>
-                        <textarea name="experience" class="form-control" id="" cols="30" rows="10"></textarea>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="">Bio</label>
-                        <textarea name="bio" class="form-control" id="" cols="30" rows="10"></textarea>
-                    </div>
-
-                    <div class="form-group">
-                        <button class="btn btn-success" type="submit">Update</button>
-                    </div>
-
-                </div>
+                    @if(Session::has('message'))
+                        <div class="alert alert-success">
+                            {{ Session::get('message') }}
+                        </div>
+                    @endif
+                </form>
             </div>
         </div>
 
@@ -41,20 +47,22 @@
                     details of user
                 </div>
             </div>
-
+            <br>
             <div class="card">
                 <div class="card-header">Update coverletter</div>
                 <div class="card-body">
                     <input type="file" class="form-control" name="cover_letter">
-                    <button class="btn btn-success">Update</button>
+                    <br>
+                    <button class="btn btn-success float-right">Update</button>
                 </div>
             </div>
-
+            <br>
             <div class="card">
                 <div class="card-header">Update resume</div>
                 <div class="card-body">
                     <input type="file" class="form-control" name="resume">
-                    <button class="btn btn-success">Update</button>
+                    <br>
+                    <button class="btn btn-success float-right">Update</button>
                 </div>
             </div>
         </div>
