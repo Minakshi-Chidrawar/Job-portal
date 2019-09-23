eae114314e67e4e5ece47b661f48489d59b2dd17
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-09-23 21:12:42 +0100

    Amended to add the guarded

--- app/Company.php
+++ app/Company.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
+    protected $guarded = [];
+
     public function jobs()
     {
         return $this->hasMany('App\Job');
