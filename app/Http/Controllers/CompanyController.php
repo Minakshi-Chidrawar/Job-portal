4755f377b861924677a57255e07f1b7864d90ece
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-09-24 20:43:35 +0100

    Amended to create company details

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Job;
 use App\Company;
 use Illuminate\Http\Request;
 
@@ -10,5 +11,24 @@ class CompanyController extends Controller
     public function index($id, Company $company)
     {
         return view ('company.index', compact('company'));
+    }
+
+    public function create()
+    {
+        return view('company.create');
+    }
+
+    public function store(Request $request)
+    {
+        $user_id = auth()->user()->id;
+        Company::where('user_id', $user_id)->update([
+            'address' => request('address'),
+            'phone' => request('phone'),
+            'website' => request('website'),
+            'slogan' => request('slogan'),
+            'description' => request('description')
+        ]);
+
+        return redirect()->back()->with('message', 'Company details are Successfully Updated!');
     }
 }
