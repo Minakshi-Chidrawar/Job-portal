fd8e10145217d0528d4f750ccf4c528525ad14f4
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-09-25 21:08:36 +0100

    Added route to upload cover photo

--- resources/views/company/create.blade.php
+++ resources/views/company/create.blade.php
@@ -42,23 +42,23 @@
                     <div class="card-body">
                         <div class="form-group">
                             <label for="">Address</label>
-                            <input type="text" class="form-control" name="address">
+                            <input type="text" class="form-control" name="address" value="{{ Auth::user()->company->address }}">
                         </div>
                         <div class="form-group">
                             <label for="">Phone</label>
-                            <input type="text" class="form-control" name="phone">
+                            <input type="text" class="form-control" name="phone" value="{{ Auth::user()->company->phone }}">
                         </div>
                         <div class="form-group">
                             <label for="">Website</label>
-                            <input type="text" class="form-control" name="website">
+                            <input type="text" class="form-control" name="website" value="{{ Auth::user()->company->website }}">
                         </div>
                         <div class="form-group">
                             <label for="">Slogan</label>
-                            <input type="text" class="form-control" name="slogan">
+                            <input type="text" class="form-control" name="slogan" value="{{ Auth::user()->company->slogan }}">
                         </div>
                         <div class="form-group">
                             <label for="">Description</label>
-                            <textarea name="description" class="form-control" id="" cols="30" rows="10"></textarea>
+                            <textarea name="description" class="form-control" id="" cols="30" rows="5">{{ Auth::user()->company->description }}</textarea>
                         </div>
                         <div class="form-group">
                             <button class="btn btn-success" type="submit">Update</button>
@@ -77,17 +77,17 @@
                     <p>Company Phone: {{ Auth::user()->company->phone }}</p>
                     <p>Company Website: <a href="{{ Auth::user()->company->website }}">{{ Auth::user()->company->website }}</a></p>
                     <p>Company Slogan: {{ Auth::user()->company->slogan }}</p>
-                    <p>Company Description: {{ Auth::user()->company->description }}</p>
+                    <p>Company Page: <a href="company/{{ Auth::user()->company->slug }}">View</a></p>
                 </div>
             </div>
 
             <br>
-            <form action="{{ route('cover.letter') }}" method="POST" enctype="multipart/form-data">
+            <form action="{{ route('cover.photo') }}" method="POST" enctype="multipart/form-data">
                 @csrf
                 <div class="card">
                     <div class="card-header">Update coverletter</div>
                     <div class="card-body">
-                        <input type="file" class="form-control" name="cover_letter">
+                        <input type="file" class="form-control" name="cover_photo">
                         <br>
                         <button class="btn btn-success float-right">Update</button>
 
