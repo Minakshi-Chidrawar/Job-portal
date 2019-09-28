568deea5d37cec829a3c3fb6a01f2708e547134c
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-09-28 23:58:23 +0100

    Action to add job details

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Job;
+use App\Company;
 use Illuminate\Http\Request;
 
 class JobController extends Controller
@@ -20,6 +21,27 @@ class JobController extends Controller
 
     public function create()
     {
+        return view('jobs.create');
+    }
+
+    public function store(Request $request)
+    {
+        $user_id = auth()->user()->id;
+        $company = Company::where('user_id', $user_id)->first();
+        $company_id = $company->id;
+
+        Job::create([
+            'user_id' => $user_id,
+            'company_id' => $company_id,
+            'title' => request('title'),
+            'slug' =>str_slug(request('title')),
+            'description' => request('description'),
+            'roles' => request('roles'),
+            'category_id' => request('category'),
+            'position' => request('position'),
+            
+        ]);
+
         return view('jobs.create');
     }
 }
