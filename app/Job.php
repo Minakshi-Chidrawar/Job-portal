30a154c0ae84297e19a1754c325413553f3b0365
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-10-06 20:08:24 +0100

    Added users and checkApplication function

--- app/Job.php
+++ app/Job.php
@@ -17,4 +17,14 @@ class Job extends Model
     {
         return $this->belongsTo('App\Company');
     }
+    
+    public function users()
+    {
+        return $this->belongsToMany(User::class)->withTimeStamps();
+    }
+
+    public function checkApplication()
+    {
+        return \DB::table('job_user')->where('user_id', auth()->user()->id)->where('job_id', $this->id)->exists();
+    }
 }
